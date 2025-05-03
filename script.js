73bca946ec5fd09511b2ff9178a76fd5ba758734
lingeshwaranpkt <lingeshwaranpkt@gmail.com>
2025-05-03 13:03:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 // Function to toggle the visibility of event details
 function toggleDetails(eventId) {
-    // Get the event details element by its ID
     const detailsElement = document.getElementById(eventId);
 
-    // Toggle the display of the event details (hide/show)
     if (detailsElement.style.display === "block") {
         detailsElement.style.display = "none";
     } else {
         detailsElement.style.display = "block";
     }
 }
-
